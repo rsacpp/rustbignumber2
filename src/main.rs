e1beaaf91e630b18ce96a6f24f3dccab92fb8b5c
rsacpp <rsacpp@gmail.com>
2025-10-20 12:33:12 -0400

    remove cargo lock

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use env_logger::Builder;
 use log::LevelFilter;
-// use log::info;
+use std::time::{Duration, SystemTime};
+
+use log::info;
 mod bn64;
 use bn64::Bn64;
 
@@ -11,15 +13,17 @@ use bn128::Bn128;
 fn main() {
     Builder::new().filter_level(LevelFilter::Info).init();
 
-    let mersenne: usize = 2203;
+    let mersenne: usize = 2281;
     let mut p = bn64::mersenne(mersenne);
 
     p.to_hex();
     let mut p_1 = p.clone();
     p_1.sub_at(0, 1);
     let mut bn0: Bn64 = Bn64::from(String::from("abcdef0123456789"));
-
+    let start = SystemTime::now();
     let r = bn64::npmod2(&mut bn0, &mut p_1, &mut p);
+    let end = SystemTime::now();
+    info!("Time elapsed {:?}", end.duration_since(start));
     r.to_hex();
     /*
      let r0 = bn64::npmod(&mut bn0, &mut p_1, &mut p);
@@ -30,6 +34,9 @@ fn main() {
     let mut p_3 = p_2.clone();
     p_3.sub_at(0, 1);
     let mut b128: Bn128 = Bn128::from(String::from("abcdef0123456789"));
+    let start = SystemTime::now();
     let r = bn128::npmod2(&mut b128, &mut p_3, &mut p_2);
+    let end = SystemTime::now();
+    info!("Time elapsed {:?}", end.duration_since(start));
     r.to_hex();
 }
